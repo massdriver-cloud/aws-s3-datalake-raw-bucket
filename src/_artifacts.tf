308c7b560c45063ce43111030057023448fbea82
amy null <null@amy.gg>
2023-03-31 16:07:21 -0400

    ✨ feature: Initial commit!

---
+++ src/_artifacts.tf
@@ -0,0 +1,32 @@
+resource "massdriver_artifact" "bucket" {
+  field                = "bucket"
+  provider_resource_id = aws_s3_bucket.main.arn
+  name                 = "AWS S3 Bucket: ${aws_s3_bucket.main.arn}"
+  artifact = jsonencode(
+    {
+      data = {
+        infrastructure = {
+          arn = aws_s3_bucket.main.arn
+        }
+        security = {
+          iam = {
+            read = {
+              policy_arn = aws_iam_policy.read.arn
+            }
+            write = {
+              policy_arn = aws_iam_policy.write.arn
+            }
+            athenaread = {
+              policy_arn = aws_iam_policy.athena_read.arn
+            }
+          }
+        }
+      }
+      specs = {
+        aws = {
+          region = var.bucket.region
+        }
+      }
+    }
+  )
+}
---
+++ src/main.tf
@@ -0,0 +1,40 @@
+resource "aws_s3_bucket" "main" {
+  bucket        = var.md_metadata.name_prefix
+  force_destroy = false
+}
+
+resource "aws_s3_bucket_acl" "private" {
+  bucket = aws_s3_bucket.main.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_public_access_block" "main" {
+  bucket = aws_s3_bucket.main.id
+
+  block_public_acls   = true
+  block_public_policy = true
+  ignore_public_acls  = true
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "main" {
+  bucket = aws_s3_bucket.main.id
+
+  rule {
+    id     = "s3-raw-transition"
+    status = "Enabled"
+
+    transition {
+      days          = var.bucket.lifecycle.transfer_s3_ia
+      storage_class = "STANDARD_IA"
+    }
+
+    transition {
+      days          = var.bucket.lifecycle.transfer_s3_glacier
+      storage_class = "GLACIER"
+    }
+
+    expiration {
+      days = var.bucket.lifecycle.expiration
+    }
+  }
+}
