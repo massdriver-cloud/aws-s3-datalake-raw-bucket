7b0fe7d8d654122e314e59c018c940e59b705408
chrisghill <chris@massdriver.cloud>
2023-10-24 13:54:51 -0600

    Enforce bucket privacy (for SOC2)

--- src/main.tf
+++ src/main.tf
@@ -3,17 +3,21 @@ resource "aws_s3_bucket" "main" {
   force_destroy = false
 }
 
-resource "aws_s3_bucket_acl" "private" {
+resource "aws_s3_bucket_ownership_controls" "main" {
   bucket = aws_s3_bucket.main.id
-  acl    = "private"
+  rule {
+    # this setting disables ACLs
+    object_ownership = "BucketOwnerEnforced"
+  }
 }
 
 resource "aws_s3_bucket_public_access_block" "main" {
   bucket = aws_s3_bucket.main.id
 
-  block_public_acls   = true
-  block_public_policy = true
-  ignore_public_acls  = true
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 resource "aws_s3_bucket_lifecycle_configuration" "main" {
