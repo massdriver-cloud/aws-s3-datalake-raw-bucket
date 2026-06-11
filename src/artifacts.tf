3513fe9ba8d750e5f80e5b2e9dd8fccbaae4f050
chrisghill <chris@massdriver.cloud>
2026-06-11 15:55:04 -0600

    remove top-level 'data' on all artifact-definitions

--- src/artifacts.tf
+++ src/artifacts.tf
@@ -3,21 +3,19 @@ resource "massdriver_artifact" "bucket" {
   name     = "AWS S3 Bucket: ${aws_s3_bucket.main.arn}"
   artifact = jsonencode(
     {
-      data = {
-        infrastructure = {
-          arn = aws_s3_bucket.main.arn
-        }
-        security = {
-          iam = {
-            read = {
-              policy_arn = aws_iam_policy.read.arn
-            }
-            write = {
-              policy_arn = aws_iam_policy.write.arn
-            }
-            athena_read = {
-              policy_arn = aws_iam_policy.athena_read.arn
-            }
+      infrastructure = {
+        arn = aws_s3_bucket.main.arn
+      }
+      security = {
+        iam = {
+          read = {
+            policy_arn = aws_iam_policy.read.arn
+          }
+          write = {
+            policy_arn = aws_iam_policy.write.arn
+          }
+          athena_read = {
+            policy_arn = aws_iam_policy.athena_read.arn
           }
         }
       }
